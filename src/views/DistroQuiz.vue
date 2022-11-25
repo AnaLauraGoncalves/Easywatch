b841cf469568c032fb91037d31df5d170a46374b
AnaLauraGoncalves <analauragoncalves85818@gmail.com>
2022-11-25 13:54:03 -0300

    feat:

--- src/views/DistroQuiz.vue
+++ src/views/DistroQuiz.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="DistroQuiz container">
-    <DistroQuizVue :distroList="distroList" />
+    <DistroQuizVue />
   </div>
 </template>
 
 <script>
 import DistroQuizVue from "@/components/DistroQuizVue.vue";
-import Distros from "@/assets/distros.json";
 
 export default {
   name: "DistroQuiz",
-  data:{
-    distroList: Distros,
-  },
   components: {
     DistroQuizVue,
   },
